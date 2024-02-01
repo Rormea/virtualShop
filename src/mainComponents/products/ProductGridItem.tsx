03dfaac97e8a25e7a12584e42ef30c7069154c5c
Ronald <rormeas@gmail.com>
2024-02-01 13:08:28 -0500

    detail images

--- src/mainComponents/products/ProductGridItem.tsx
+++ src/mainComponents/products/ProductGridItem.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 
 
@@ -8,15 +12,35 @@ interface Props {
 }
 
 export const ProductGridItem = ({ product }: Props) => {
+
+    const [displayImage, setDisplayImage] = useState(product.images[0])
+
     return (
         <div className="rounded-md overflow-hidden fade-in" >
-            <Image
-                src={`/products/${product.images[0]}`}
-                alt={product.title}
-                className="w-full object-cover"
-                width={500}
-                height={500}
-            />
+
+            <Link href={`/product/${product.slug}`}>
+                <Image
+                    src={`/products/${displayImage}`}
+                    alt={product.title}
+                    className="w-full object-cover rounded-sm"
+                    width={500}
+                    height={500}
+                    onMouseEnter={() => setDisplayImage(product.images[1])}
+                    onMouseLeave={() => setDisplayImage(product.images[0])}
+                />
+            </Link>
+
+
+            <div className="p-4 flex flex-col" >
+                <Link
+                    className="hover:text-blue-700 hover:font-semibold"
+                    href={`/product/${product.slug}`}
+                >
+                    {product.title}
+                </Link>
+
+                <span className="font-bold" >${product.price}</span>
+            </div>
         </div>
     )
 }
