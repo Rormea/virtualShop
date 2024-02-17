d7707a9dd9aa773848200b483d6a7cdb6289a1a8
Ronald <rormeas@gmail.com>
2024-02-17 15:03:47 -0500

    store zustand for show sideMenu

--- src/mainComponents/ui/sidebar/SideBar.tsx
+++ src/mainComponents/ui/sidebar/SideBar.tsx
@@ -1,30 +1,55 @@
 'use client'
 
 
+
+import { useUIStore } from '@/store'
+import clsx from 'clsx'
 import Link from 'next/link'
 import React from 'react'
 import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from 'react-icons/io5'
 
 export const SideBar = () => {
 
+    const sideMenuIsOpen = useUIStore(state => state.isSideMenuOpen);
+    const closeMenu = useUIStore(state => state.closeSideMenu);
+
 
     return (
         <div className=''>
 
             {/* background */}
-            <div className='fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-50' ></div>
+            {
+                sideMenuIsOpen && (
+                    <div className='fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-50' ></div>
+                )
+            }
+
 
             {/* Blur */}
-            <div className='fade-in fixed top-0 left-0 w-screen h-screen z-10  backdrop-filter backdrop-blur-sm' ></div>
+            {
+                sideMenuIsOpen && (
+                    <div className='fade-in fixed top-0 left-0 w-screen h-screen z-10  backdrop-filter backdrop-blur-sm'
+                        onClick={closeMenu}
+                    ></div>
+                )
+            }
+
 
             {/* Side Menu */}
             <nav
-                className='fixed p-5 right-0 top-0 w-[400px] h-screen bg-slate-200 z-20 shadow-2xl transform transition-all duration-300'
+                className={
+                    clsx(
+                        'fixed p-5 right-0 top-0 w-[400px] h-screen bg-slate-200 z-20 shadow-2xl transform transition-all duration-300',
+                        {
+                            "translate-x-full": !sideMenuIsOpen
+                        }
+                    )
+                }
             >
                 <IoCloseOutline
                     size={50}
                     className='absolute top-5 right-5 cursor-pointer'
-                    onClick={() => console.log("Hago click se debe cerrar")}
+                    onClick={() => closeMenu()}
                 />
 
                 {/* Input*/}
@@ -40,22 +65,22 @@ export const SideBar = () => {
                 </div>
 
                 {/* Menu*/}
-                <Link href='/' className='flex items-center mt-10 p-2  hover:bg-gray-100 rounded transition-all'>
+                <Link href='/' className='flex items-center mt-10 p-2  hover:bg-gray-300 rounded transition-all'>
                     <IoPersonOutline size={30} />
                     <span className='ml-3 text-xl' >Perfil</span>
                 </Link>
 
-                <Link href='/' className='flex items-center mt-10 p-2  hover:bg-gray-100 rounded transition-all'>
+                <Link href='/' className='flex items-center mt-10 p-2  hover:bg-gray-300 rounded transition-all'>
                     <IoTicketOutline size={30} />
                     <span className='ml-3 text-xl' >Ordenes</span>
                 </Link>
 
-                <Link href='/' className='flex items-center mt-10 p-2  hover:bg-gray-100 rounded transition-all'>
+                <Link href='/' className='flex items-center mt-10 p-2  hover:bg-gray-300 rounded transition-all'>
                     <IoLogInOutline size={30} />
                     <span className='ml-3 text-xl' >Entrar</span>
                 </Link>
 
-                <Link href='/' className='flex items-center mt-10 p-2  hover:bg-gray-100 rounded transition-all'>
+                <Link href='/' className='flex items-center mt-10 p-2  hover:bg-gray-300 rounded transition-all'>
                     <IoLogOutOutline size={30} />
                     <span className='ml-3 text-xl' >Salir</span>
                 </Link>
@@ -63,12 +88,12 @@ export const SideBar = () => {
                 {/* Line separator*/}
                 <div className='w-full h-px bg-gray-500 my-10' ></div>
 
-                <Link href='/' className='flex items-center mt-10 p-2  hover:bg-gray-100 rounded transition-all'>
+                <Link href='/' className='flex items-center mt-10 p-2  hover:bg-gray-300 rounded transition-all'>
                     <IoShirtOutline size={30} />
                     <span className='ml-3 text-xl' >Productos</span>
                 </Link>
 
-                <Link href='/' className='flex items-center mt-10 p-2  hover:bg-gray-100 rounded transition-all'>
+                <Link href='/' className='flex items-center mt-10 p-2  hover:bg-gray-300 rounded transition-all'>
                     <IoTicketOutline size={30} />
                     <span className='ml-3 text-xl' >Pedidos</span>
                 </Link>
@@ -76,6 +101,6 @@ export const SideBar = () => {
 
 
             </nav>
-        </div>
+        </div >
     )
 }
--- src/mainComponents/ui/top-menu/TopMenu.tsx
+++ src/mainComponents/ui/top-menu/TopMenu.tsx
@@ -1,9 +1,18 @@
+"use client"
+
 import { titleFont } from "@/config/fonts"
+import { useUIStore } from "@/store";
 import Link from "next/link"
 import { IoCartOutline, IoSearchOutline } from "react-icons/io5"
 
 
 export const TopMenu = () => {
+
+
+
+    const openMenu = useUIStore(state => state.openSideMenu);
+
+
     return (
         <nav className="flex px-5 justify-between items-center w-ful" >
 
@@ -39,7 +48,9 @@ export const TopMenu = () => {
                     </div>
                 </Link>
 
-                <button className="m-2 rounded-md transition-all hover:bg-gray-100" >
+                <button className="m-2 rounded-md transition-all hover:bg-gray-100"
+                    onClick={() => openMenu()}
+                >
                     Menú
                 </button>
             </div>
